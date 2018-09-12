62e35ea6c08e33a85d8ed8ef6674893c8d9dcd9f
alexlhanson <alexlhanson@gmail.com>
2018-09-12 15:35:44 -0700

    Add initial listeners and user creations

--- lab-alex/chatroom.js
+++ lab-alex/chatroom.js
@@ -1,11 +1,62 @@
 'use strict';
 
+// Requires
 const net = require('net');
+const ee = require('events');
 
+//3rd Party Requires
+const uuid = require('uuid');
+
+//server env
 const port = process.env.port || 3000;
-
 const server = net.createServer();
+const events = new ee();
 
+//start up server
 server.listen(port, () => {
   console.log(`server up and running on port: ${port}`);
 });
+
+//user pool and constructor
+
+const userPool = {};
+
+let User = function(socket){
+  let id = uuid();
+  this.id = id;
+  this.nickname = `User : <${id}>`;
+  this.socket = socket;
+
+};
+
+//setup listener
+
+server.on('connection', (socket) => {
+  console.log('opened socket');
+
+  //create new user on connection socket and add to user pool
+  let user = new User(socket);
+  userPool[user.id];
+
+  //create listener for incoming data
+  socket.on('data', (buffer) => {
+    messageDispatch(user.nickname, buffer);
+  });
+});
+
+//logic for emitting a message
+let messageDispatch = (userName, buffer) => {
+  let message = parseOut(buffer);
+  events.emit();
+};
+
+//message parser
+let parseOut = (buffer) => {
+  messageBody = buffer.toString();
+
+
+}
+
+/********************************************************************************
+*         user features                                                         *
+********************************************************************************/
